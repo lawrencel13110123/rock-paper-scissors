fbcd890b826d90b10f0c542c58a39ba2d301a672
Spencer Charest <spencercharest@Spencers-MacBook-Pro.local>
2016-08-09 10:15:09 -0700

    started js, finished function to checkWinner

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,30 @@
 $(document).ready(function(){
-
-
+  var userName1;
+  var userName2;
+  var userChoice1;
+  var userChoice2;
 });
+
+function checkWinner(){
+  if(userChoice1 === userChoice2){
+    alert('It\'s a tie!');
+  } else if(userChoice1 === 'rock'){
+      if(userChoice2 === 'paper'){
+        alert(userName2 + 'wins!');
+      } else{
+        alert(userName1 + 'wins!');
+      }
+  } else if(userChoice1 === 'paper'){
+      if(userChoice2 === 'rock'){
+        alert(userName1 + 'wins!');
+      } else{
+        alert(userName2 + 'wins!');
+      }
+  } else{
+      if(userChoice2 === 'rock'){
+        alert(userName2 + 'wins!');
+      } else{
+        alert(userName1 + 'wins!');
+      }
+  }
+}
