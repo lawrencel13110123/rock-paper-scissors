cbd5c63fd095de4ce372e06e60a2ac5c554e64fd
Spencer Charest <spencercharest@Spencers-MacBook-Pro.local>
2016-08-11 11:05:31 -0700

    starting over with presence

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,15 +5,33 @@ $(document).ready(function(){
     databaseURL: "https://rock-paper-scissors-50d2e.firebaseio.com",
     storageBucket: "rock-paper-scissors-50d2e.appspot.com",
   };
+  var playercount;
   firebase.initializeApp(config);
   database = firebase.database();
 
-  $('#submit-button').on('click', function(){
-    console.log('hit');
-    var name = $('#name').val().trim();
-    console.log(name);
-    database.ref('players').set({
-      name: name
-    });
+  // connectionsRef references a secific location in our database.
+  // All of our connections will be stored in this directory.
+  var connectionsRef = database.ref("/connections");
+
+  // '.info/connected' is a special location provided by Firebase that is updated every time the client's connection state changes.
+  // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+  var connectedRef = database.ref(".info/connected");
+
+  // When the client's connection state changes...
+  connectedRef.on("value", function(snap) {
+
+  	// If they are connected..
+  	if( snap.val() ) {
+
+  		// Add user to the connections list.
+  		var con = connectionsRef.push(true);
+      console.log(connectionsRef);
+  		// Remove user from the connection list when they disconnect.
+  		con.onDisconnect().remove();
+
+  	};
+
   });
+
+
 });
