83c6ba25351d27916c266ff3589101d85aa3c2a8
Spencer Charest <spencercharest@Spencers-MacBook-Pro.local>
2016-08-11 13:49:47 -0700

    starting over

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,26 +5,10 @@ $(document).ready(function(){
     databaseURL: "https://rock-paper-scissors-50d2e.firebaseio.com",
     storageBucket: "rock-paper-scissors-50d2e.appspot.com",
   };
-  playerNumber = 1;
+  var playerCount;
   firebase.initializeApp(config);
   var database = firebase.database();
-  var connectedRef = database.ref(".info/connected");
-  var connectionsRef = database.ref("/players");
 
-$('#submit-button').on('click', function(){
-  var name = $('#name').val().trim();
-  connectionsRef.push(true);
-});
-
-// When the client's connection state changes...
-connectedRef.on("value", function(snap) {
-	// If they are connected..
-	if( snap.val() ) {
-		// Remove user from the connection list when they disconnect.
-		connectionsRef.onDisconnect().remove();
-	};
-
-});
 
 
 });
