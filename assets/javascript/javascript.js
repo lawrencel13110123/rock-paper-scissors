478a00f7297ddbca07d7808fc14ccae89e4d1ada
Spencer Charest <spencercharest@Spencers-MacBook-Pro.local>
2016-08-11 12:58:13 -0700

    starting over again

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,33 +5,3 @@ $(document).ready(function(){
     databaseURL: "https://rock-paper-scissors-50d2e.firebaseio.com",
     storageBucket: "rock-paper-scissors-50d2e.appspot.com",
   };
-  var playercount;
-  firebase.initializeApp(config);
-  database = firebase.database();
-
-  // connectionsRef references a secific location in our database.
-  // All of our connections will be stored in this directory.
-  var connectionsRef = database.ref("/connections");
-
-  // '.info/connected' is a special location provided by Firebase that is updated every time the client's connection state changes.
-  // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
-  var connectedRef = database.ref(".info/connected");
-
-  // When the client's connection state changes...
-  connectedRef.on("value", function(snap) {
-
-  	// If they are connected..
-  	if( snap.val() ) {
-
-  		// Add user to the connections list.
-  		var con = connectionsRef.push(true);
-      console.log(connectionsRef);
-  		// Remove user from the connection list when they disconnect.
-  		con.onDisconnect().remove();
-
-  	};
-
-  });
-
-
-});
