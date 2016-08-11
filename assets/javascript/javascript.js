73b35fa4d29f26a5e0356c19e2dd4be7f3a2c639
Spencer Charest <spencercharest@Spencers-MacBook-Pro.local>
2016-08-11 08:23:42 -0700

    added Firebase

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+  var config = {
+    apiKey: "AIzaSyAaMZAJo1Ua3PC8RAbRV9yFluO0zUrbg18",
+    authDomain: "rock-paper-scissors-50d2e.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-50d2e.firebaseio.com",
+    storageBucket: "rock-paper-scissors-50d2e.appspot.com",
+  };
   var userName1 = 'Spencer';
   var userName2 = 'Leah';
   var userChoice1 = '';
